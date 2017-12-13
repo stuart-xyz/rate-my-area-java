43e302b10fd610dfd3478f85e0256473bff2f01d
Stuart Phillips <stuart.p@livingmap.com>
2017-12-13 01:33:35 +0000

    feat: add login endpoint

--- app/controllers/AuthController.java
+++ app/controllers/AuthController.java
@@ -3,10 +3,12 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import services.AuthService;
 
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.Optional;
 
 
@@ -27,8 +29,15 @@ public class AuthController extends Controller {
   public Result login() {
     JsonNode json = request().body().asJson();
     UserLoginData userLoginData = Json.fromJson(json, UserLoginData.class);
-    final Optional<String> cookieOption = authService.login(userLoginData.email, userLoginData.password);
-    return ok(cookieOption.orElse("cookie not found"));
+    final Optional<Http.Cookie> cookieOption = authService.login(userLoginData.email, userLoginData.password);
+    final HashMap<String, String> responseJson = new HashMap<>();
+    if (cookieOption.isPresent()) {
+      responseJson.put("message", "Log in successful");
+      return ok(Json.toJson(responseJson));
+    } else {
+      responseJson.put("error", "Invalid login credentials provided");
+      return unauthorized(Json.toJson(responseJson));
+    }
   }
 
 }
--- app/services/AuthService.java
+++ app/services/AuthService.java
@@ -2,15 +2,21 @@ package services;
 
 import models.User;
 import org.mindrot.jbcrypt.BCrypt;
+import play.cache.SyncCacheApi;
+import play.mvc.Http;
 
 import javax.inject.Inject;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.util.Base64;
 import java.util.Optional;
+import java.util.UUID;
 
 public class AuthService {
 
   private final DatabaseService databaseService;
+  private final SyncCacheApi cacheApi;
   private final MessageDigest mda;
   private final String cookieHeader = "X-Auth-Token";
 
@@ -20,17 +26,28 @@ public class AuthService {
   }
 
   @Inject
-  public AuthService(DatabaseService databaseService) throws NoSuchAlgorithmException {
+  public AuthService(DatabaseService databaseService, SyncCacheApi cacheApi) throws NoSuchAlgorithmException {
     this.databaseService = databaseService;
+    this.cacheApi = cacheApi;
     this.mda = MessageDigest.getInstance("SHA-512");
   }
 
-  public Optional<String> login(String email, String password) {
+  public Optional<Http.Cookie> login(String email, String password) {
     final Optional<User> userOption = this.databaseService.getUserOption(email);
     return userOption.map(user -> {
-      if (BCrypt.checkpw(password, user.hashedPassword)) return "cookie";
+      if (BCrypt.checkpw(password, user.hashedPassword)) return generateCookie(user);
       else return null;
     });
+  }
+
+  private Http.Cookie generateCookie(User user) {
+    final String randomPart = UUID.randomUUID().toString().toUpperCase();
+    final String userPart = user.id.toString().toUpperCase();
+    final String key = String.format("%s|%s", randomPart, userPart);
+    final String token = Base64.getEncoder().encodeToString(mda.digest(key.getBytes()));
+    final Integer duration = (int) Duration.ofHours(10).getSeconds();
+    cacheApi.set(token, user, duration);
+    return new Http.Cookie(cookieHeader, token, duration, "/", null, false, true, Http.Cookie.SameSite.STRICT);
   }
 
 }
