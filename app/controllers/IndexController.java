c7d5abd15b9a66bf3e6638087a4f2e29a7737adc
Stuart Phillips <stuart.p@livingmap.com>
2017-12-26 18:00:26 +0000

    feat: integrate UI

--- app/controllers/IndexController.java
+++ app/controllers/IndexController.java
@@ -1,12 +1,23 @@
 package controllers;
 
-import play.mvc.*;
+import play.filters.csrf.CSRF;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import javax.inject.Inject;
 
 
 public class IndexController extends Controller {
 
+  private final CSRF.TokenProviderProvider tokenProviderProvider;
+
+  @Inject
+  public IndexController(CSRF.TokenProviderProvider tokenProviderProvider) {
+    this.tokenProviderProvider = tokenProviderProvider;
+  }
+
   public Result index() {
-    return ok(views.html.index.render());
+    return ok(views.html.index.render(tokenProviderProvider.get().generateToken()));
   }
 
 }
