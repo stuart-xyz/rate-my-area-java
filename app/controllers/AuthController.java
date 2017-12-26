53a30b73cf1ac20259a779b288909acb7e5453c1
Stuart Phillips <stuart.p@livingmap.com>
2017-12-26 15:47:34 +0000

    feat: add validation to login method

--- app/controllers/AuthController.java
+++ app/controllers/AuthController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.data.Form;
+import play.data.FormFactory;
+import play.data.validation.Constraints;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
@@ -19,10 +22,20 @@ import java.util.Optional;
 public class AuthController extends Controller {
 
   private final AuthService authService;
+  private final FormFactory formFactory;
 
   public static class UserLoginData {
-    public String email;
+    private String email;
     public String password;
+
+    @Constraints.Email
+    public String getEmail() {
+      return this.email;
+    }
+
+    public void setEmail(String email) {
+      this.email = email;
+    }
   }
 
   public static class UserSignupData {
@@ -32,22 +45,33 @@ public class AuthController extends Controller {
   }
 
   @Inject
-  public AuthController(AuthService authService) {
+  public AuthController(AuthService authService, FormFactory formFactory) {
     this.authService = authService;
+    this.formFactory = formFactory;
   }
 
   public Result login() {
-    JsonNode json = request().body().asJson();
-    UserLoginData userLoginData = Json.fromJson(json, UserLoginData.class);
-    final Optional<Http.Cookie> cookieOption = authService.login(userLoginData.email, userLoginData.password);
     final HashMap<String, String> responseJson = new HashMap<>();
-    if (cookieOption.isPresent()) {
-      responseJson.put("message", "Log in successful");
-      response().setCookie(cookieOption.get());
-      return ok(Json.toJson(responseJson));
+    Optional<JsonNode> jsonOption = Optional.ofNullable(request().body().asJson());
+    if (jsonOption.isPresent()) {
+      Form<UserLoginData> userLoginForm = formFactory.form(UserLoginData.class).bind(jsonOption.get());
+      if (userLoginForm.hasErrors()) {
+        responseJson.put("error", "Expected email and password");
+        return badRequest(Json.toJson(responseJson));
+      } else {
+        final Optional<Http.Cookie> cookieOption = authService.login(userLoginForm.get().email, userLoginForm.get().password);
+        if (cookieOption.isPresent()) {
+          responseJson.put("message", "Log in successful");
+          response().setCookie(cookieOption.get());
+          return ok(Json.toJson(responseJson));
+        } else {
+          responseJson.put("error", "Invalid login credentials provided");
+          return unauthorized(Json.toJson(responseJson));
+        }
+      }
     } else {
-      responseJson.put("error", "Invalid login credentials provided");
-      return unauthorized(Json.toJson(responseJson));
+      responseJson.put("error", "Expected JSON body");
+      return badRequest(Json.toJson(responseJson));
     }
   }
 
