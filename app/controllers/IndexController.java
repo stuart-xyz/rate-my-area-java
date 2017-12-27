cd9a99d71884adfc2ef96ffcd689df7a58840217
Stuart Phillips <stuart.p@livingmap.com>
2017-12-27 01:49:52 +0000

    fix: CSRF token generation

--- app/controllers/IndexController.java
+++ app/controllers/IndexController.java
@@ -4,20 +4,11 @@ import play.filters.csrf.CSRF;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import javax.inject.Inject;
-
 
 public class IndexController extends Controller {
 
-  private final CSRF.TokenProviderProvider tokenProviderProvider;
-
-  @Inject
-  public IndexController(CSRF.TokenProviderProvider tokenProviderProvider) {
-    this.tokenProviderProvider = tokenProviderProvider;
-  }
-
   public Result index() {
-    return ok(views.html.index.render(tokenProviderProvider.get().generateToken()));
+    return ok(views.html.index.render(CSRF.getToken(request()).get().value()));
   }
 
 }
