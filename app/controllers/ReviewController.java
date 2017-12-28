0240a5f2157a8b13c6da2d26cdebb596ddd2a341
Stuart Phillips <stuart.p@livingmap.com>
2017-12-28 22:40:38 +0000

    refactor: make all database calls asynchronous

--- app/controllers/ReviewController.java
+++ app/controllers/ReviewController.java
@@ -9,6 +9,7 @@ import play.data.Form;
 import play.data.FormFactory;
 import play.data.validation.Constraints;
 import play.libs.Json;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
@@ -21,10 +22,13 @@ import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 
 public class ReviewController extends Controller {
 
+  private final HttpExecutionContext httpExecutionContext;
   private final FormFactory formFactory;
   private final DatabaseService databaseService;
   private final AbstractS3Service s3Service;
@@ -61,21 +65,22 @@ public class ReviewController extends Controller {
   }
 
   @Inject
-  public ReviewController(FormFactory formFactory, DatabaseService databaseService, AbstractS3Service s3Service) {
+  public ReviewController(HttpExecutionContext httpExecutionContext, FormFactory formFactory, DatabaseService databaseService, AbstractS3Service s3Service) {
+    this.httpExecutionContext = httpExecutionContext;
     this.formFactory = formFactory;
     this.databaseService = databaseService;
     this.s3Service = s3Service;
   }
 
   @With(UserAuthAction.class)
-  public Result create() {
+  public CompletionStage<Result> create() {
     final HashMap<String, String> responseJson = new HashMap<>();
     Optional<JsonNode> jsonOption = Optional.ofNullable(request().body().asJson());
     if (jsonOption.isPresent()) {
       final Form<ReviewFormData> reviewForm = formFactory.form(ReviewFormData.class).bind(jsonOption.get());
       if (reviewForm.hasErrors()) {
         responseJson.put("Error", "Invalid data supplied");
-        return badRequest(Json.toJson(responseJson));
+        return CompletableFuture.completedFuture(badRequest(Json.toJson(responseJson)));
       }
 
       final User user = (User) ctx().args.get("user");
@@ -84,34 +89,38 @@ public class ReviewController extends Controller {
       reviewFormData.imageUrls.forEach(url -> review.imageUrls.add(new ImageUrl(url)));
       user.reviews.add(review);
 
-      try {
-        user.save();
-      } catch(Exception e) {
-        responseJson.put("Error", "Unexpected internal error occurred");
-        return internalServerError(Json.toJson(responseJson));
-      }
+      return CompletableFuture.supplyAsync(() -> {
+        try {
+          user.save();
+        } catch(Exception e) {
+          responseJson.put("Error", "Unexpected internal error occurred");
+          return internalServerError(Json.toJson(responseJson));
+        }
 
-      responseJson.put("message", "Review added successfully");
-      return ok(Json.toJson(responseJson));
+        responseJson.put("message", "Review added successfully");
+        return ok(Json.toJson(responseJson));
+      }, this.httpExecutionContext.current());
 
     } else {
       responseJson.put("Error", "Expected JSON body");
-      return badRequest(Json.toJson(responseJson));
+      return CompletableFuture.completedFuture(badRequest(Json.toJson(responseJson)));
     }
   }
 
   @With(UserAuthAction.class)
-  public Result list() {
-    final HashMap<String, String> responseJson = new HashMap<>();
-    final List<DisplayedReview> reviews;
-    try {
-      reviews = this.databaseService.listReviews();
-    } catch (Exception e) {
-      responseJson.put("Error", "Unexpected internal error occurred");
-      return internalServerError(Json.toJson(responseJson));
-    }
+  public CompletionStage<Result> list() {
+    return CompletableFuture.supplyAsync(() -> {
+      final HashMap<String, String> responseJson = new HashMap<>();
+      final List<DisplayedReview> reviews;
+      try {
+        reviews = this.databaseService.listReviews();
+      } catch (Exception e) {
+        responseJson.put("Error", "Unexpected internal error occurred");
+        return internalServerError(Json.toJson(responseJson));
+      }
 
-    return ok(Json.toJson(reviews));
+      return ok(Json.toJson(reviews));
+    }, this.httpExecutionContext.current());
   }
 
   private boolean userOwnsReview(User user, Long reviewId) throws CustomExceptions.ReviewNotFoundException {
@@ -120,33 +129,15 @@ public class ReviewController extends Controller {
   }
 
   @With(UserAuthAction.class)
-  public Result edit(Long id) {
-    final User user = (User) ctx().args.get("user");
-    final HashMap<String, String> responseJson = new HashMap<>();
-    try {
-      if (!userOwnsReview(user, id)) {
-        responseJson.put("error", "Not authorised to edit this review");
-        return unauthorized(Json.toJson(responseJson));
-      }
-    } catch (CustomExceptions.ReviewNotFoundException e) {
-      responseJson.put("error", "Review does not exist");
-      return notFound(Json.toJson(responseJson));
-    } catch (Exception e) {
-      responseJson.put("error", "Unexpected internal error occurred");
-      return internalServerError(Json.toJson(responseJson));
-    }
-
-    Optional<JsonNode> jsonOption = Optional.ofNullable(request().body().asJson());
-    if (jsonOption.isPresent()) {
-      final Form<ReviewEditData> reviewEditForm = formFactory.form(ReviewEditData.class).bind(jsonOption.get());
-      if (reviewEditForm.hasErrors()) {
-        responseJson.put("Error", "Invalid data supplied");
-        return badRequest(Json.toJson(responseJson));
-      }
-
-      final ReviewEditData reviewEditData = reviewEditForm.get();
+  public CompletionStage<Result> edit(Long id) {
+    return CompletableFuture.supplyAsync(() -> {
+      final User user = (User) ctx().args.get("user");
+      final HashMap<String, String> responseJson = new HashMap<>();
       try {
-        this.databaseService.updateReview(id, new Review(reviewEditData.title, reviewEditData.areaName, reviewEditData.description));
+        if (!userOwnsReview(user, id)) {
+          responseJson.put("error", "Not authorised to edit this review");
+          return unauthorized(Json.toJson(responseJson));
+        }
       } catch (CustomExceptions.ReviewNotFoundException e) {
         responseJson.put("error", "Review does not exist");
         return notFound(Json.toJson(responseJson));
@@ -155,47 +146,69 @@ public class ReviewController extends Controller {
         return internalServerError(Json.toJson(responseJson));
       }
 
-      responseJson.put("message", "Review updated successfully");
-      return ok(Json.toJson(responseJson));
+      Optional<JsonNode> jsonOption = Optional.ofNullable(request().body().asJson());
+      if (jsonOption.isPresent()) {
+        final Form<ReviewEditData> reviewEditForm = formFactory.form(ReviewEditData.class).bind(jsonOption.get());
+        if (reviewEditForm.hasErrors()) {
+          responseJson.put("Error", "Invalid data supplied");
+          return badRequest(Json.toJson(responseJson));
+        }
 
-    } else {
-      responseJson.put("Error", "Expected JSON body");
-      return badRequest(Json.toJson(responseJson));
-    }
+        final ReviewEditData reviewEditData = reviewEditForm.get();
+        try {
+          this.databaseService.updateReview(id, new Review(reviewEditData.title, reviewEditData.areaName, reviewEditData.description));
+        } catch (CustomExceptions.ReviewNotFoundException e) {
+          responseJson.put("error", "Review does not exist");
+          return notFound(Json.toJson(responseJson));
+        } catch (Exception e) {
+          responseJson.put("error", "Unexpected internal error occurred");
+          return internalServerError(Json.toJson(responseJson));
+        }
+
+        responseJson.put("message", "Review updated successfully");
+        return ok(Json.toJson(responseJson));
+
+      } else {
+        responseJson.put("Error", "Expected JSON body");
+        return badRequest(Json.toJson(responseJson));
+      }
+    }, this.httpExecutionContext.current());
   }
 
   @With(UserAuthAction.class)
-  public Result delete(Long id) {
-    final User user = (User) ctx().args.get("user");
-    final HashMap<String, String> responseJson = new HashMap<>();
-    try {
-      if (!userOwnsReview(user, id)) {
-        responseJson.put("error", "Not authorised to edit this review");
-        return unauthorized(Json.toJson(responseJson));
+  public CompletionStage<Result> delete(Long id) {
+    return CompletableFuture.supplyAsync(() -> {
+      final User user = (User) ctx().args.get("user");
+      final HashMap<String, String> responseJson = new HashMap<>();
+      try {
+        if (!userOwnsReview(user, id)) {
+          responseJson.put("error", "Not authorised to edit this review");
+          return unauthorized(Json.toJson(responseJson));
+        }
+      } catch (CustomExceptions.ReviewNotFoundException e) {
+        responseJson.put("error", "Review does not exist");
+        return notFound(Json.toJson(responseJson));
+      } catch (Exception e) {
+        responseJson.put("error", "Unexpected internal error occurred");
+        return internalServerError(Json.toJson(responseJson));
       }
-    } catch (CustomExceptions.ReviewNotFoundException e) {
-      responseJson.put("error", "Review does not exist");
-      return notFound(Json.toJson(responseJson));
-    } catch (Exception e) {
-      responseJson.put("error", "Unexpected internal error occurred");
-      return internalServerError(Json.toJson(responseJson));
-    }
 
-    final Review currentReview;
-    try {
-      currentReview = this.databaseService.getReview(id);
-      this.databaseService.deleteReview(id);
-    } catch (CustomExceptions.ReviewNotFoundException e) {
-      responseJson.put("error", "Review does not exist");
-      return notFound(Json.toJson(responseJson));
-    } catch (Exception e) {
-      responseJson.put("error", "Unexpected internal error occurred");
-      return internalServerError(Json.toJson(responseJson));
-    }
-    currentReview.imageUrls.forEach(imageUrl -> this.s3Service.delete(imageUrl.url));
+      final Review currentReview;
+      try {
+        currentReview = this.databaseService.getReview(id);
+        this.databaseService.deleteReview(id);
+      } catch (CustomExceptions.ReviewNotFoundException e) {
+        responseJson.put("error", "Review does not exist");
+        return notFound(Json.toJson(responseJson));
+      } catch (Exception e) {
+        responseJson.put("error", "Unexpected internal error occurred");
+        return internalServerError(Json.toJson(responseJson));
+      }
+      currentReview.imageUrls.forEach(imageUrl -> this.s3Service.delete(imageUrl.url));
 
-    responseJson.put("message", "Review deleted successfully");
-    return ok(Json.toJson(responseJson));
+      responseJson.put("message", "Review deleted successfully");
+      return ok(Json.toJson(responseJson));
+    }, this.httpExecutionContext.current());
   }
 
 }
--- app/controllers/UploadController.java
+++ app/controllers/UploadController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.User;
 import play.libs.Json;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -13,29 +14,36 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
 public class UploadController extends Controller {
 
+  private final HttpExecutionContext httpExecutionContext;
   private final AbstractS3Service s3Service;
 
   @Inject
-  public UploadController(AbstractS3Service s3Service) {
+  public UploadController(HttpExecutionContext httpExecutionContext, AbstractS3Service s3Service) {
+    this.httpExecutionContext = httpExecutionContext;
     this.s3Service = s3Service;
   }
 
   @With(UserAuthAction.class)
-  public Result upload() {
+  public CompletionStage<Result> upload() {
     final User user = (User) ctx().args.get("user");
     final Http.MultipartFormData<File> formData = request().body().asMultipartFormData();
-    final List<String> urls = formData.getFiles().stream().map(file -> {
-      String filename = UUID.randomUUID().toString();
-      return this.s3Service.upload(file.getFile(), filename, user.id);
-    }).collect(Collectors.toList());
 
-    final HashMap<String, List<String>> responseJson = new HashMap<>();
-    responseJson.put("urls", urls);
-    return ok(Json.toJson(responseJson));
+    return CompletableFuture.supplyAsync(() -> {
+      final List<String> urls = formData.getFiles().stream().map(file -> {
+        String filename = UUID.randomUUID().toString();
+        return this.s3Service.upload(file.getFile(), filename, user.id);
+      }).collect(Collectors.toList());
+
+      final HashMap<String, List<String>> responseJson = new HashMap<>();
+      responseJson.put("urls", urls);
+      return ok(Json.toJson(responseJson));
+    }, this.httpExecutionContext.current());
   }
 
 }
